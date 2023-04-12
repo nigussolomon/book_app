8d2b2927408088e00ffd0d795fb68a151efbe999
Aklogsirak <aklogsirak1902@gmail.com>
2023-04-12 09:09:15 +0000

    added home and detail page layout

--- lib/pages/Details.dart
+++ lib/pages/Details.dart
@@ -1,40 +1,93 @@
+// import 'package:flutter/material.dart';
+// import 'package:book_app/resources/model.dart';
+// import 'Home.dart';
+
+// class DetailPage extends StatelessWidget {
+//   final Book book;
+
+//   const DetailPage({super.key, required this.book});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Book Details'),
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           Text(
+//             book.name,
+//             style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+//           ),
+//           SizedBox(height: 10),
+//           Text(
+//             'By ' + book.author,
+//             style: TextStyle(fontSize: 20),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class Book {
+//   final String name;
+//   final String author;
+
+//   Book({required this.name, required this.author});
+// }
+
 import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+
+// import 'package:cinema/models/movies.dart';
+// import '../../utils/constant.dart';
+// import '../home/body.dart';
+// import 'package:cinema/screens/detail/body.dart';
+import 'package:book_app/resources/model.dart';
 
 import 'Home.dart';
 
-class DetailPage extends StatelessWidget {
-final Book book;
+class DetailsScreen extends StatelessWidget {
+  final Book book21;
+  const DetailsScreen({Key? key, required this.book21}) : super(key: key);
 
-DetailPage({required this.book});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: book21.color,
+      appBar: buildAppBar(context),
+      // body: Body(movie: book21),
+      // body: DetailsScreen(book21: book21),
+    );
+  }
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text('Book Details'),
-),
-body: Column(
-mainAxisAlignment: MainAxisAlignment.center,
-crossAxisAlignment: CrossAxisAlignment.center,
-children: [
-Text(
-book.name,
-style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-),
-SizedBox(height: 10),
-Text(
-'By ' + book.author,
-style: TextStyle(fontSize: 20),
-     ),
-    ],
-   ),
-  );
- }
-}
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: book21.color,
+      elevation: 0,
+      leading: IconButton(
+        icon: Icon(Icons.logout),
+        onPressed: () => Navigator.pop(context),
+      ),
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.person),
+        ),
 
-class Book {
-final String name;
-final String author;
-
-Book({required this.name, required this.author});
+        // IconButton(
+        //   onPressed: (){},
+        //   icon: SvgPicture.asset(
+        //     "assets/icons/cart.svg",
+        //   ),
+        // ),
+        SizedBox(
+          width: 22 / 2,
+        ),
+      ],
+    );
+  }
 }
--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,37 +1,187 @@
-import 'package:flutter/material.dart';
-
-import 'Details.dart';
+import "package:flutter/material.dart";
+import "Details.dart";
 
 class HomePage extends StatelessWidget {
-final List books = [
-Book(name: 'The Alchemist', author: 'Paulo Coelho'),
-Book(name: 'To Kill a Mockingbird', author: 'Harper Lee'),
-Book(name: 'The Catcher in the Rye', author: 'J.D. Salinger'),
-];
-
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: const Text('Book App'),
-),
-body: ListView.builder(
-itemCount: books.length,
-itemBuilder: (BuildContext context, int index) {
-return ListTile(
-title: Text(books[index].name),
-subtitle: Text(books[index].author),
-onTap: () {
-Navigator.push(
-context,
-MaterialPageRoute(
-builder: (context) => DetailPage(book: books[index]),
-        ),
-       );
-      },
-     );
-    },
-   ),
-  );
- }
+  final Book book1;
+  final search = TextEditingController();
+  HomePage({Key? key, required this.book1}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              title: Text(
+                "Books",
+                style: TextStyle(color: Colors.black),
+              ),
+              actions: [
+                Icon(
+                  Icons.people,
+                  color: Colors.black,
+                )
+              ],
+              backgroundColor: Colors.white,
+              elevation: 0,
+            ),
+            body: Container(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(7),
+                  child: TextField(
+                    controller: search,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.elliptical(7, 7))),
+                      hintText: "Search",
+                      labelText: "Search",
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: GridView.builder(
+                      itemCount: books.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisExtent: 22 * 10,
+                        crossAxisSpacing: 22,
+                        childAspectRatio: 0.7,
+                      ),
+                      itemBuilder: (context, index) => ItemCard(
+                        book: books[index],
+                        // press: () {},
+                        press: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailsScreen(
+                                book21: books[index],
+                              ),
+                            )),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ))));
+  }
 }
+
+class ItemCard extends StatelessWidget {
+  final Book book;
+  final VoidCallback press;
+  // required this.movie, required this.press,
+  ItemCard({
+    required this.book,
+    required this.press,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(22),
+              // height: 170,
+              // width: 150,
+              decoration: BoxDecoration(
+                // color: ,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Hero(tag: "${book.id}", child: Image.asset(book.image)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 22 / 4),
+            child: Text(
+              book.title,
+              style: TextStyle(color: Colors.amber, fontSize: 20),
+            ),
+          ),
+          Text("\$${book.price}",
+              style: TextStyle(
+                  color: Colors.amber,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15)),
+        ],
+      ),
+    );
+  }
+}
+
+class Book {
+  final String image, title, description;
+  final num price, time, id;
+  final Color color;
+
+  Book({
+    required this.id,
+    required this.image,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.color,
+    required this.time,
+  });
+}
+
+List<Book> books = [
+  Book(
+      id: 1,
+      image: "assets/images/the_catcher_in_the_rye.jpg",
+      title: "The Catcher in the rye",
+      price: 20,
+      description: "dummyText",
+      color: Color.fromARGB(255, 92, 92, 92),
+      time: 130),
+  Book(
+      id: 2,
+      image: "assets/images/the_catcher_in_the_rye.jpg",
+      title: "The Catcher in the rye",
+      price: 20,
+      description: "dummyText",
+      color: Color.fromARGB(255, 138, 117, 117),
+      time: 130),
+  Book(
+      id: 3,
+      image: "assets/images/the_catcher_in_the_rye.jpg",
+      title: "The Catcher in the rye",
+      price: 20,
+      description: "dummyText",
+      color: Color.fromARGB(255, 212, 131, 38),
+      time: 130),
+  Book(
+      id: 4,
+      image: "assets/images/the_catcher_in_the_rye.jpg",
+      title: "The Catcher in the rye",
+      price: 20,
+      description: "dummyText",
+      color: Color.fromARGB(255, 50, 67, 68),
+      time: 130),
+  Book(
+      id: 5,
+      image: "assets/images/the_catcher_in_the_rye.jpg",
+      title: "The Catcher in the rye",
+      price: 20,
+      description: "dummyText",
+      color: Color.fromARGB(255, 51, 49, 49),
+      time: 130),
+  Book(
+      id: 6,
+      image: "assets/images/the_catcher_in_the_rye.jpg",
+      title: "The Catcher in the rye",
+      price: 20,
+      description: "dummyText",
+      color: Color.fromARGB(255, 231, 103, 43),
+      time: 130)
+];
