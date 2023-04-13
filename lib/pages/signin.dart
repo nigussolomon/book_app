a80afaefd273e0ba895afb7972b91729887137c4
yeabseras7 <yeabseraseyoum569@gmail.com>
2023-04-13 14:29:04 +0300

    Sign in UI error fixed

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -20,59 +20,64 @@ class _SigninState extends State<Signin> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "SIGN IN",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              child: TextFormField(
-                decoration: const InputDecoration(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 70),
+              const Text(
+                "SIGN IN",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.elliptical(7, 7))),
+                      hintText: 'Enter your name',
+                      labelText: "Name"),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                child: TextField(
+                  obscureText: passwordVisible,
+                  decoration: const InputDecoration(
                     border: OutlineInputBorder(
                         borderRadius:
                             BorderRadius.all(Radius.elliptical(7, 7))),
-                    hintText: 'Enter your name',
-                    labelText: "Name"),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              child: TextField(
-                obscureText: passwordVisible,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.elliptical(7, 7))),
-                  hintText: 'Choose a Password',
-                  labelText: "Password",
+                    hintText: 'Choose a Password',
+                    labelText: "Password",
+                  ),
                 ),
               ),
-            ),
-            Container(
-                height: 90,
-                width: MediaQuery.of(context).size.width,
-                padding: const EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                      backgroundColor: const MaterialStatePropertyAll(
-                          Color.fromARGB(255, 91, 180, 253)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(7.0),
-                      ))),
-                  child: const Text("Continue"),
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Doesn't have an account? "),
-                TextButton(onPressed: () {}, child: const Text("Sign up"))
-              ],
-            )
-          ],
+              Container(
+                  height: 70,
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: const MaterialStatePropertyAll(
+                            Color.fromARGB(255, 91, 180, 253)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ))),
+                    child: const Text("Continue"),
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Doesn't have an account? "),
+                  TextButton(onPressed: () {}, child: const Text("Sign up"))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
