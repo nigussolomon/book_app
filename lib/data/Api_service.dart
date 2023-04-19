0334545dc33c145260028975383dd04ba61049c1
kaleab <808kaleabkzin@gmail.com>
2023-04-19 10:00:17 +0300

    added search api request and test

--- lib/data/Api_service.dart
+++ lib/data/Api_service.dart
@@ -1,20 +1,44 @@
+import 'package:book_app/data/book.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Service {
-  List Books = [];
-  Future fetchBooks() async {
-    final response = await http.get(
-        Uri.parse('https://https://book-api-au20.onrender.com/.com/api/books'));
+  //Future fetchBooks() async {
+  //  final response = await http
+  //      .get(Uri.parse('https://book-api-au20.onrender.com/.com/api/books'));
+  //  if (response.statusCode == 200) {
+  //    final data = json.decode(response.body);
+  //    final List books = [];
+  //    for (var book in data) {
+  //      Books.add(book.fromJson(book));
+  //    }
+  //    return books;
+  //  } else {
+  //    throw Exception('Failed to load books');
+  //  }
+  //}
+
+  Future<List<Book>> searchBooks(String param) async {
+    var queryParameters = {
+      'bookname': param,
+      'authorname': param,
+    };
+    var uri =
+        Uri.https('book-api-au20.onrender.com', '/search', queryParameters);
+    var response = await http.get(uri);
+    //print(response.request);
+
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      final List books = [];
+      //print(data);
+      final List<Book> books = [];
       for (var book in data) {
-        Books.add(book.fromJson(book));
+        books.add(Book.fromJson(book));
       }
       return books;
     } else {
-      throw Exception('Failed to load books');
+      //print(response.body);
+      throw Exception("failed to search books");
     }
   }
 }
---
+++ test/api_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:book_app/data/Api_service.dart';
+import 'package:book_app/data/book.dart';
+
+import 'dart:developer' as dev;
+
+void main() async {
+  test('api must search for books given a param', () async {
+    final service = Service();
+
+    List<Book> result = await service.searchBooks("chip8");
+
+    dev.log("hello world");
+    //print("result: $result");
+
+    assert(result.isNotEmpty);
+  });
+}
+
+// void testSearchApi() {
+  // group('search api', () {
+    //test();
+  // });
+// }
